b5a11d3ef592e0894b51909d368bfec7e79dd30d
liaoc611 <liaoc611@newschool.edu>
2026-02-17 15:35:02 -0500

    final upload

--- Project1/Homepage/homepage.js
+++ Project1/Homepage/homepage.js
@@ -15,3 +15,31 @@ regions.forEach(region => {
   });
 
 });
+
+const asciiFrame = document.querySelector(".ascii-frame");
+const fullText = document.getElementById("fullText");
+const closeBtn = document.getElementById("closeFullText");
+
+asciiFrame.addEventListener("click", () => {
+  fullText.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  fullText.classList.remove("active");
+});
+
+const landing = document.getElementById("landing");
+const mainContent = document.getElementById("main-content");
+
+function enterSite() {
+  landing.classList.add("hidden");
+  mainContent.classList.add("visible");
+
+  setTimeout(() => {
+    landing.remove();
+  }, 700);
+}
+
+landing.addEventListener("click", enterSite);
+
+setTimeout(enterSite, 30000);
